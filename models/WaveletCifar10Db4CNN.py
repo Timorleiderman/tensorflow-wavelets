d46c634fe07e096012903fdcab901be87333f490
Timorleiderman <Timorleiderman@gmail.com>
2021-06-16 00:03:01 +0300

    Create WaveletCifar10Db4CNN.py

---
+++ models/WaveletCifar10Db4CNN.py
@@ -0,0 +1,49 @@
+from keras.models import Model
+from models.DWT import DWT_Pooling, IWT_UpSampling, DWT_Pooling_Db4 ,IWT_UpSampling_Db4
+from keras.layers import Input, Conv2D, MaxPooling2D, Dense, Flatten, BatchNormalization, Activation, Dropout
+
+
+def WaveletDb4CNN(input_size=(32, 32, 3), nb_classes=10):
+    inputs = Input(shape=input_size)
+
+    output = Conv2D(32, (3, 3), padding="same")(inputs)
+    output = BatchNormalization()(output)
+    output = Activation("relu")(output)
+
+    output = Conv2D(32, (3, 3), padding="same")(output)
+    output = BatchNormalization()(output)
+    output = Activation("relu")(output)
+
+    output = DWT_Pooling_Db4()(output)  # dwt
+
+    output = Dropout(0.5)(output)  # added dropout to improve overfitting
+
+    output = Conv2D(32, (3, 3), padding="same")(output)
+
+    output = Dropout(0.25)(output)
+
+    output = Conv2D(64, (3, 3), padding="same")(output)
+    output = BatchNormalization()(output)
+    output = Activation("relu")(output)
+
+    output = Conv2D(64, (3, 3), padding="same")(output)
+    output = BatchNormalization()(output)
+    output = Activation("relu")(output)
+
+    output = DWT_Pooling_Db4()(output)  # dwt
+
+    output = Dropout(0.5)(output)  # added dropout to improve overfitting
+
+    output = Conv2D(64, (3, 3), padding="same")(output)
+
+    output = Flatten()(output)
+    output = Dropout(0.25)(output)
+
+    output = Dense(512, activation="relu")(output)
+    output = Dropout(0.5)(output)
+
+    output = Dense(nb_classes, activation="softmax")(output)
+
+    model = Model(inputs=inputs, outputs=output)
+
+    return model
