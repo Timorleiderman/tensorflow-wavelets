6065fcb783ec7864b694a845c6a83fcb62049aa6
Timorleiderman <Timorleiderman@gmail.com>
2021-07-15 17:26:38 +0300

    working on idwt

--- matlab/scripts/test.m
+++ matlab/scripts/test.m
@@ -2,7 +2,8 @@ clear
 
 f = imread('../../input/LennaGrey.png');
 % f = double(rgb2gray(f));
-wavelet_name = 'haar';
+f = double(f);
+wavelet_name = 'db2';
 
 [LoD,HiD] = wfilters(wavelet_name,'d'); % decomposition
 [LoR,HiR] = wfilters(wavelet_name,'r'); % reconstruction
@@ -13,9 +14,9 @@ reconstructed = idwt2(cA,cH,cV,cD,LoR,HiR);
 [cAmy,cHmy,cVmy,cDmy] = mydwt2d(f,LoD,HiD);
 myreconstructed = myidwt2d(cAmy,cHmy,cVmy,cDmy,LoR,HiR);
 
-disperr2d(myreconstructed,reconstructed)
+disperr2d(myreconstructed,reconstructed);
 
-figure(1)
-imshow(uint8(myreconstructed))
-figure(2)
-imshow(uint8(reconstructed))
+% figure(1)
+% imshow(uint8(myreconstructed))
+% figure(2)
+% imshow(uint8(reconstructed))
--- models/DWT2.py
+++ models/DWT2.py
@@ -1,9 +1,16 @@
+import os
+import cv2
 import math
 import pywt
+import numpy as np
+from utils import mse
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.datasets import mnist, cifar10
+
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # for tensor flow warning
+# os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
 
 class DWT(layers.Layer):
@@ -19,10 +26,10 @@ class DWT(layers.Layer):
         db2_hpf = wavelet.dec_hi
 
         # covert filters into tensors and reshape for convolution math
-        db2_lpf = tf.constant(db2_lpf)
+        db2_lpf = tf.constant(db2_lpf[::-1])
         self.db2_lpf = tf.reshape(db2_lpf, (1, wavelet.dec_len, 1, 1))
 
-        db2_hpf = tf.constant(db2_hpf)
+        db2_hpf = tf.constant(db2_hpf[::-1])
         self.db2_hpf = tf.reshape(db2_hpf, (1, wavelet.dec_len, 1, 1))
 
         self.conv_type = "VALID"
@@ -47,9 +54,9 @@ class DWT(layers.Layer):
         d = tf.nn.conv2d(
             inputs_pad, self.db2_hpf, padding=self.conv_type, strides=[1, 1, 1, 1],
         )
-        # ds - downsample
+        # ds - down sample
         a_ds = a[:, :, 1:a.shape[2]:2, :]
-        d_ds = d[:, :, 1:a.shape[2]:2, :]
+        d_ds = d[:, :, 1:d.shape[2]:2, :]
 
         # border padding symatric add rows
         a_ds_pad = tf.pad(a_ds, [[0, 0], [self.dec_len-1, self.dec_len-1], [0, 0], [0, 0]], self.border_padd)
@@ -85,9 +92,9 @@ class DWT(layers.Layer):
 
         # down sample
         ll = aa[:, 1:aa.shape[1]:2, :, :]
-        lh = ad[:, 1:aa.shape[1]:2, :, :]
-        hl = da[:, 1:aa.shape[1]:2, :, :]
-        hh = dd[:, 1:aa.shape[1]:2, :, :]
+        lh = ad[:, 1:ad.shape[1]:2, :, :]
+        hl = da[:, 1:da.shape[1]:2, :, :]
+        hh = dd[:, 1:dd.shape[1]:2, :, :]
 
         # concate all outputs ionto tensor
         x = tf.concat([ll, lh, hl, hh], axis=-1)
@@ -100,6 +107,7 @@ class IDWT(layers.Layer):
         super(IDWT, self).__init__(**kwargs)
         self._name = self.name + "_" + name
         self.pad_type = "VALID"
+        self.border_pad = "SYMMETRIC"
 
         # get filter coeffs from 3rd party lib
         wavelet = pywt.Wavelet(name)
@@ -110,10 +118,10 @@ class IDWT(layers.Layer):
         db2_hpf = wavelet.rec_hi
 
         # covert filters into tensors and reshape for convolution math
-        db2_lpf = tf.constant(db2_lpf)
+        db2_lpf = tf.constant(db2_lpf[::-1])
         self.db2_lpf = tf.reshape(db2_lpf, (1, wavelet.rec_len, 1, 1))
 
-        db2_hpf = tf.constant(db2_hpf)
+        db2_hpf = tf.constant(db2_hpf[::-1])
         self.db2_hpf = tf.reshape(db2_hpf, (1, wavelet.rec_len, 1, 1))
 
     def upsampler2d(self, x):
@@ -123,7 +131,7 @@ class IDWT(layers.Layer):
         :return:  up sampled tensor with shape (?, 2*w, 2*h, ch)
         """
         # create zero like tensor
-        zero_tensor = tf.zeros_like(x, dtype=tf.float32)
+        zero_tensor = tf.zeros_like(x)
         # stack both tensors
         stack_rows = tf.stack([x, zero_tensor], axis=3)
         # reshape for zero insertion between the rows
@@ -131,7 +139,7 @@ class IDWT(layers.Layer):
         # transpose in order to insert zeros for the columns
         stack_rows = tf.transpose(stack_rows, perm=[0, 2, 1, 3])
         # create zero like tensor but now like the padded one
-        zero_tensor_1 = tf.zeros_like(stack_rows, dtype=tf.float32)
+        zero_tensor_1 = tf.zeros_like(stack_rows)
         # stack both tensors
         stack_rows_cols = tf.stack([stack_rows, zero_tensor_1], axis=3)
         # reshape for zero insertion between the columns
@@ -145,9 +153,15 @@ class IDWT(layers.Layer):
         # border padding for convolution with low pass and high pass filters
         x = tf.pad(inputs,
                    [[0, 0], [self.rec_len-1, self.rec_len-1], [self.rec_len-1, self.rec_len-1], [0, 0]],
-                   "SYMMETRIC")
+                   self.border_pad)
+
         # convert to float32
-        x = tf.cast(x, tf.float32)
+        # x = tf.cast(x, tf.float32)
+        # GPU works with float 32
+        # CPU  can work with 64 but need to add extra flag
+        # convert to float64
+        # x = tf.cast(x, tf.float64)
+
         # extract approximation and details from input tensor
         # TODO: whit if tensor shape is bigger then 4?
         # and expand the dims for the up sampling
@@ -162,12 +176,13 @@ class IDWT(layers.Layer):
         hh_us_pad = self.upsampler2d(hh)
 
         # convolution for the rows
-        ll_conv_lpf = tf.nn.conv2d(ll_us_pad, self.db2_lpf, padding=self.pad_type, strides=[1, 1, 1, 1], )
         # transpose for the column convolution
-        ll_conv_lpf_tr = tf.transpose(ll_conv_lpf, perm=[0, 2, 1, 3])
         # convolution for the column
-        ll_conv_lpf_lpf = tf.nn.conv2d(ll_conv_lpf_tr, self.db2_lpf, padding=self.pad_type, strides=[1, 1, 1, 1], )
         # transpose back to normal
+
+        ll_conv_lpf = tf.nn.conv2d(ll_us_pad, self.db2_lpf, padding=self.pad_type, strides=[1, 1, 1, 1], )
+        ll_conv_lpf_tr = tf.transpose(ll_conv_lpf, perm=[0, 2, 1, 3])
+        ll_conv_lpf_lpf = tf.nn.conv2d(ll_conv_lpf_tr, self.db2_lpf, padding=self.pad_type, strides=[1, 1, 1, 1], )
         ll_conv_lpf_lpf_tr = tf.transpose(ll_conv_lpf_lpf, perm=[0, 2, 1, 3])
 
         lh_conv_lpf = tf.nn.conv2d(lh_us_pad, self.db2_lpf, padding=self.pad_type, strides=[1, 1, 1, 1], )
@@ -186,12 +201,12 @@ class IDWT(layers.Layer):
         hh_conv_hpf_hpf_tr = tf.transpose(hh_conv_hpf_hpf, perm=[0, 2, 1, 3])
 
         # add all together
-        ll_lh = tf.math.add(ll_conv_lpf_lpf_tr, lh_conv_lpf_hpf_tr)
-        hl_hh = tf.math.add(hl_conv_hpf_lpf_tr, hh_conv_hpf_hpf_tr)
-
-        reconstructed = tf.math.add(ll_lh, hl_hh)
+        reconstructed = tf.add_n([ll_conv_lpf_lpf_tr,
+                                  lh_conv_lpf_hpf_tr,
+                                  hl_conv_hpf_lpf_tr,
+                                  hh_conv_hpf_hpf_tr])
         # crop the paded part
-        crop = (self.rec_len - 1)*2
+        crop = (self.rec_len -1)*2
         return reconstructed[:, crop-1:-crop, crop-1:-crop, :]
 
 
@@ -204,10 +219,57 @@ if __name__ == "__main__":
     #     x_train[0, :, :, :], 0, name=None
     # )
     # print("frog shape", frog.shape)
+    # model = keras.Sequential()
+    # model.add(keras.Input(shape=(256, 256, 4)))
+    # model.add(IDWT())
+    # model.summary()
+
+    name = "db2"
+    img = cv2.imread("../input/LennaGrey.png",0)
+    img_ex1 = np.expand_dims(img, axis=-1)
+    img_ex2 = np.expand_dims(img_ex1, axis=0)
+
     model = keras.Sequential()
-    model.add(keras.Input(shape=(256, 256, 4)))
-    model.add(IDWT())
+    model.add(layers.InputLayer(input_shape=img_ex1.shape))
+    model.add(DWT(name=name))
+    # model.summary()
+    coeffs = model.predict(img_ex2)
+    LL = coeffs[0, ..., 0]
+    LH = coeffs[0, ..., 1]
+    HL = coeffs[0, ..., 2]
+    HH = coeffs[0, ..., 3]
+
+    model = keras.Sequential()
+    model.add(layers.InputLayer(input_shape=coeffs[0].shape))
+    model.add(IDWT(name=name))
     model.summary()
+
+    my_recon = model.predict(coeffs)
+    img_my_rec = my_recon[0, :, :, 0]
+    coeffs2 = pywt.wavedec2(img, name,level=1)
+
+    LL2 = coeffs2[0]
+    LH2 = coeffs2[1][0]
+    HL2 = coeffs2[1][1]
+    HH2 = coeffs2[1][2]
+
+    recon_pywt = pywt.waverec2(coeffs2, name)
+    img_pywt_rec = recon_pywt
+
+    print("LL mse ", mse.mse(LL, LL2))
+    print("LH mse ", mse.mse(LH, LH2))
+    print("HL mse ", mse.mse(HL, HL2))
+    print("HH mse ", mse.mse(HH, HH2))
+    print("img mse ", mse.mse(img_pywt_rec, img_my_rec))
+
+    difference = cv2.absdiff(np.int32(img_my_rec), np.int32(img_pywt_rec))
+    _, mask = cv2.threshold(difference.astype("uint8"), 0, 255, cv2.THRESH_BINARY)
+
+    cv2.imshow("diff", mask)
+    cv2.waitKey(0)
+    pass
+
+
 
 
 
