0804a3c9050203c0e80254b61d7890421605b28c
Timorleiderman <Timorleiderman@gmail.com>
2021-08-03 13:40:30 +0300

    complex wavelet transform

--- matlab/sym_dwt/mydwt2d.m
+++ matlab/sym_dwt/mydwt2d.m
@@ -2,15 +2,7 @@ function w = mydwt2d(x, J, LoD, HiD)
 
 for k = 1:J
     
-    [x, H] = analysis_filter_bank2d(x, LoD(:), HiD(:));
-
-    [x, w{k}{1}] = analysis_filter_bank2d(x', LoD(:), HiD(:));
-    [w{k}{2}, w{k}{3}] = analysis_filter_bank2d(H', LoD(:), HiD(:));
-
-x = x';
-w{k}{1} = w{k}{1}';
-w{k}{2} = w{k}{2}';
-w{k}{3} = w{k}{3}';
+    [x, w{k}{1}, w{k}{2}, w{k}{3}] = analysis_filter_bank2d(x, LoD(:), HiD(:));
 
 end
 w{J+1} = x;
