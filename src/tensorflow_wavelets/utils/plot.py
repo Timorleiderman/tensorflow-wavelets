287d3e5b2652d9488cbcab167ce700689b8d69ef
Timorleiderman <Timorleiderman@gmail.com>
2021-10-04 13:04:36 +0300

    Create plot.py

---
+++ src/tensorflow_wavelets/utils/plot.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_n_examples(x, y, start=0, stop=9, p=None):
+    if stop < start:
+        print("stop value mast be bigger then start")
+        return
+
+    indices = range(start, stop)
+    y = np.argmax(y, axis=1)
+    if p is None:
+        p = y
+    plt.figure(figsize=(10, 5))
+    for i, index in enumerate(indices):
+        plt.subplot(2, 5, i+1)
+        plt.imshow(x[index].reshape((28, 28)), cmap='binary')
+        plt.xticks([])
+        plt.yticks([])
+        if y[index] == p[index]:
+            col = 'g'
+        else:
+            col = 'r'
+        plt.xlabel(str(p[index]), color=col)
+    return plt
+
+
+def plot_random_examples(x, y, p=None):
+    indices = np.random.choice(range(0, x.shape[0]), 10)
+    y = np.argmax(y, axis=1)
+    if p is None:
+        p = y
+    plt.figure(figsize=(10, 5))
+    for i, index in enumerate(indices):
+        plt.subplot(2, 5, i+1)
+        plt.imshow(x[index].reshape((28, 28)), cmap='binary')
+        plt.xticks([])
+        plt.yticks([])
+        if y[index] == p[index]:
+            col = 'g'
+        else:
+            col = 'r'
+        plt.xlabel(str(p[index]), color=col)
+    return plt
