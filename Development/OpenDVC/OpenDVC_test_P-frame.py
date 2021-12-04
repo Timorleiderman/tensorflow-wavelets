e3c89991c8b2bc4eccda0b0f63af6c269c0397f1
Timorleiderman <Timorleiderman@gmail.com>
2021-12-04 13:53:15 +0200

    Timor: updated opendvc work

--- Development/OpenDVC/OpenDVC_test_P-frame.py
+++ Development/OpenDVC/OpenDVC_test_P-frame.py
@@ -1,22 +1,21 @@
 import argparse
 import numpy as np
 import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras import Model
 import tensorflow_compression as tfc
 from scipy import misc
-import CNN_img
 import motion
-import MC_network
 import os
 import imageio
-
-import entropy_models
-
-tf.compat.v1.disable_eager_execution()
-
+import cv2
+# tf.executing_eagerly()
+tf.config.run_functions_eagerly(True)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-config = tf.compat.v1.ConfigProto(allow_soft_placement=True)
-sess = tf.compat.v1.Session(config=config)
+
+batch_size = 1
+Channel = 3
 
 parser = argparse.ArgumentParser(
       formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -32,79 +31,32 @@ parser.add_argument("--M", type=int, default=128, choices=[128])
 
 args = parser.parse_args()
 
-batch_size = 1
-Channel = 3
+Y0_com_img = cv2.imread(args.ref) / 255.0
+Y1_com_img = cv2.imread("/workspaces/tensorflow-wavelets/Development/OpenDVC/BasketballPass/f055.png") / 255.0
 
-Y0_com_img = imageio.imread(args.ref)
-Y1_raw_img = imageio.imread(args.raw)
+Y1_raw_img = cv2.imread(args.raw)
 
 Y0_com_img = np.expand_dims(Y0_com_img, 0)
+Y1_com_img = np.expand_dims(Y1_com_img, 0)
+
 Y1_raw_img = np.expand_dims(Y1_raw_img, 0)
 
-Height = np.size(Y1_raw_img, 1)
-Width = np.size(Y1_raw_img, 2)
+h = Y0_com_img.shape[1]
+w = Y0_com_img.shape[2]
+c = Y0_com_img.shape[3]
 
-Y0_com = tf.compat.v1.placeholder(tf.float32, [batch_size, Height, Width, Channel])
-Y1_raw = tf.compat.v1.placeholder(tf.float32, [batch_size, Height, Width, Channel])
+# Y0_com_img_tf = tf.convert_to_tensor(Y0_com_img, dtype=tf.float32)
 
-with tf.compat.v1.variable_scope("flow_motion"):
+# flow_tensor = motion.optical_flow(Y0_com_img_tf, Y0_com_img_tf, 1, h, w)
 
-    flow_tensor, _, _, _, _, _ = motion.optical_flow(Y0_com, Y1_raw, batch_size, Height, Width)
-    # Y1_warp_0 = tf.contrib.image.dense_image_warp(Y0_com, flow_tensor)
+# motion estimation
+x_inp1 = layers.Input(shape=(h, w, c))
+x_inp2 = layers.Input(shape=(h, w, c))
+x = motion.OpticalFlow()([x_inp1, x_inp2])
+vt = Model(inputs=[x_inp1, x_inp2], outputs=x, name="MyModel")
+aout = vt.predict([Y0_com_img, Y0_com_img])
+# model.summary()
 
-# Encode flow
-flow_latent = CNN_img.MV_analysis(flow_tensor, args.N, args.M)
+# motion vector out
 
-entropy_bottleneck_mv = entropy_models.EntropyBottleneck()
-string_mv = entropy_bottleneck_mv.compress(flow_latent)
-string_mv = tf.squeeze(string_mv, axis=0)
-
-flow_latent_hat, MV_likelihoods = entropy_bottleneck_mv(flow_latent, training=False)
-
-flow_hat = CNN_img.MV_synthesis(flow_latent_hat, args.N)
-
-# Motion Compensation
-Y1_warp = tf.contrib.image.dense_image_warp(Y0_com, flow_hat)
-
-MC_input = tf.concat([flow_hat, Y0_com, Y1_warp], axis=-1)
-Y1_MC = MC_network.MC(MC_input)
-
-# Encode residual
-Res = Y1_raw - Y1_MC
-
-res_latent = CNN_img.Res_analysis(Res, num_filters=args.N, M=args.M)
-
-entropy_bottleneck_res = tfc.EntropyBottleneck()
-string_res = entropy_bottleneck_res.compress(res_latent)
-string_res = tf.squeeze(string_res, axis=0)
-
-res_latent_hat, Res_likelihoods = entropy_bottleneck_res(res_latent, training=False)
-
-Res_hat = CNN_img.Res_synthesis(res_latent_hat, num_filters=args.N)
-
-# Reconstructed frame
-Y1_com = tf.clip_by_value(Res_hat + Y1_MC, 0, 1)
-
-if args.metric == 'PSNR':
-    train_mse = tf.reduce_mean(tf.squared_difference(Y1_com, Y1_raw))
-    quality = 10.0*tf.log(1.0/train_mse)/tf.log(10.0)
-elif args.metric == 'MS-SSIM':
-    quality = tf.math.reduce_mean(tf.image.ssim_multiscale(Y1_com, Y1_raw, max_val=1))
-
-saver = tf.train.Saver(max_to_keep=None)
-model_path = './OpenDVC_model/' + args.mode + '_' + str(args.l) + '_model/model.ckpt'
-saver.restore(sess, save_path=model_path)
-
-compressed_frame, string_MV, string_Res, quality_com \
-    = sess.run([Y1_com, string_mv, string_res, quality],
-               feed_dict={Y0_com: Y0_com_img / 255.0, Y1_raw: Y1_raw_img / 255.0})
-
-with open(args.bin, "wb") as ff:
-    ff.write(np.array(len(string_MV), dtype=np.uint16).tobytes())
-    ff.write(string_MV)
-    ff.write(string_Res)
-
-imageio.imsave(args.com, np.uint8(np.round(compressed_frame[0] * 255.0)))
-bpp = (2 + len(string_MV) + len(string_Res)) * 8 / Height / Width
-
-print(args.metric + ' = ' + str(quality_com), 'bpp = ' + str(bpp))
+print(aout.max(), aout.min())
--- Development/OpenDVC/motion.py
+++ Development/OpenDVC/motion.py
@@ -1,26 +1,28 @@
 import tensorflow as tf
+from tensorflow.keras.layers import AveragePooling2D, Conv2D
 import tensorflow_addons as tfa
+from tensorflow.keras import layers
 
-def convnet(im1_warp, im2, flow, layer):
 
-    with tf.compat.v1.variable_scope("flow_cnn_" + str(layer), reuse= tf.compat.v1.AUTO_REUSE):
+def convnet(im1_warp, im2, flow):
 
-        input = tf.concat([im1_warp, im2, flow], axis=-1)
+    input = tf.concat([im1_warp, im2, flow], axis=-1)
 
-        conv1 = tf.keras.layers.Conv2D(filters=32, kernel_size=[7, 7], padding="same", activation=tf.nn.relu)(input)
-        conv2 = tf.keras.layers.Conv2D(filters=64, kernel_size=[7, 7], padding="same", activation=tf.nn.relu)(conv1)
-        conv3 = tf.keras.layers.Conv2D(filters=32, kernel_size=[7, 7], padding="same", activation=tf.nn.relu)(conv2)
-        conv4 = tf.keras.layers.Conv2D(filters=16, kernel_size=[7, 7], padding="same", activation=tf.nn.relu)(conv3)
-        conv5 = tf.keras.layers.Conv2D(filters=2 , kernel_size=[7, 7], padding="same", activation=None)(conv4)
+    conv1 = Conv2D(filters=32, kernel_size=[7, 7], padding="same", activation=tf.nn.relu)(input)
+    conv2 = Conv2D(filters=64, kernel_size=[7, 7], padding="same", activation=tf.nn.relu)(conv1)
+    conv3 = Conv2D(filters=32, kernel_size=[7, 7], padding="same", activation=tf.nn.relu)(conv2)
+    conv4 = Conv2D(filters=16, kernel_size=[7, 7], padding="same", activation=tf.nn.relu)(conv3)
+    conv5 = Conv2D(filters=2 , kernel_size=[7, 7], padding="same", activation=None)(conv4)
 
     return conv5
 
 
-def loss(flow_course, im1, im2, layer):
+def loss(flow_course, im1, im2):
 
     flow = tf.image.resize(flow_course, [tf.shape(im1)[1], tf.shape(im2)[2]])
-    im1_warped = tfa.image.dense_image_warp(im1, flow)
-    res = convnet(im1_warped, im2, flow, layer)
+
+    im1_warped = tfa.image.dense_image_warp(im1, flow )
+    res = convnet(im1_warped, im2, flow)
     flow_fine = res + flow
 
     im1_warped_fine = tfa.image.dense_image_warp(im1, flow_fine)
@@ -29,24 +31,43 @@ def loss(flow_course, im1, im2, layer):
     return loss_layer, flow_fine
 
 
-def optical_flow(im1_4, im2_4, batch, h, w):
+def optical_flow(im1_4, im2_4,bach, h, w):
 
-    im1_3 = tf.keras.layers.AveragePooling2D(pool_size=2, strides=2, padding='same')(im1_4)
-    im1_2 = tf.keras.layers.AveragePooling2D(pool_size=2, strides=2, padding='same')(im1_3)
-    im1_1 = tf.keras.layers.AveragePooling2D(pool_size=2, strides=2, padding='same')(im1_2)
-    im1_0 = tf.keras.layers.AveragePooling2D(pool_size=2, strides=2, padding='same')(im1_1)
+    im1_3 = AveragePooling2D(pool_size=2, strides=2, padding='same')(im1_4)
+    im1_2 = AveragePooling2D(pool_size=2, strides=2, padding='same')(im1_3)
+    im1_1 = AveragePooling2D(pool_size=2, strides=2, padding='same')(im1_2)
+    im1_0 = AveragePooling2D(pool_size=2, strides=2, padding='same')(im1_1)
 
-    im2_3 = tf.keras.layers.AveragePooling2D(pool_size=2, strides=2, padding='same')(im2_4)
-    im2_2 = tf.keras.layers.AveragePooling2D(pool_size=2, strides=2, padding='same')(im2_3)
-    im2_1 = tf.keras.layers.AveragePooling2D(pool_size=2, strides=2, padding='same')(im2_2)
-    im2_0 = tf.keras.layers.AveragePooling2D(pool_size=2, strides=2, padding='same')(im2_1)
+    im2_3 = AveragePooling2D(pool_size=2, strides=2, padding='same')(im2_4)
+    im2_2 = AveragePooling2D(pool_size=2, strides=2, padding='same')(im2_3)
+    im2_1 = AveragePooling2D(pool_size=2, strides=2, padding='same')(im2_2)
+    im2_0 = AveragePooling2D(pool_size=2, strides=2, padding='same')(im2_1)
 
-    flow_zero = tf.zeros([batch, h//16, w//16, 2])
+    flow_zero = tf.zeros((1, h//16, w//16, 2), dtype=tf.dtypes.float32)
 
-    loss_0, flow_0 = loss(flow_zero, im1_0, im2_0, 0)
-    loss_1, flow_1 = loss(flow_0, im1_1, im2_1, 1)
-    loss_2, flow_2 = loss(flow_1, im1_2, im2_2, 2)
-    loss_3, flow_3 = loss(flow_2, im1_3, im2_3, 3)
-    loss_4, flow_4 = loss(flow_3, im1_4, im2_4, 4)
+    loss_0, flow_0 = loss(flow_zero, im1_0, im2_0)
+    loss_1, flow_1 = loss(flow_0, im1_1, im2_1)
+    loss_2, flow_2 = loss(flow_1, im1_2, im2_2)
+    loss_3, flow_3 = loss(flow_2, im1_3, im2_3)
+    loss_4, flow_4 = loss(flow_3, im1_4, im2_4)
 
     return flow_4, loss_0, loss_1, loss_2, loss_3, loss_4
+    # return im2_0
+    
+
+class OpticalFlow(layers.Layer):
+    def __init__(self, trainable=True, name=None, dtype=None, dynamic=False, **kwargs):
+        super().__init__(trainable=trainable, name=name, dtype=dtype, dynamic=dynamic, **kwargs)
+
+    def build(self, input_shape):
+
+        self.bach = input_shape[0][0]
+        self.h = input_shape[0][1]
+        self.w = input_shape[1][2]
+
+        return super().build(input_shape)
+
+    def call(self, inputs, *args, **kwargs):
+        flow_4, loss_0, loss_1, loss_2, loss_3, loss_4 = optical_flow(inputs[0], inputs[1], self.bach, self.h, self.w)
+        return flow_4
+
