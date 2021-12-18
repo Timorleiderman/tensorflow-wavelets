fe129a9f55902719aad788a544a2a18395192790
Timorleiderman <Timorleiderman@gmail.com>
2021-12-19 00:24:50 +0200

    Update motion.py

--- Development/OpenDVC/motion.py
+++ Development/OpenDVC/motion.py
@@ -5,8 +5,10 @@ from tensorflow.keras import layers
 import tensorflow_compression as tfc
 import numpy as np
 
+
 def convnet(im1_warp, im2, flow):
 
+    
     input = tf.concat([im1_warp, im2, flow], axis=-1)
 
     conv1 = Conv2D(filters=32, kernel_size=[7, 7], padding="same", activation=tf.nn.relu)(input)
@@ -20,13 +22,19 @@ def convnet(im1_warp, im2, flow):
 
 def loss(flow_course, im1, im2):
 
-    flow = tf.image.resize(flow_course, [tf.shape(im1)[1], tf.shape(im2)[2]])
+    # flow_shape = [-1, im1.shape[1], im1.shape[2], 2]
 
-    im1_warped = tfa.image.dense_image_warp(im1, flow )
+    flow = tf.image.resize(flow_course, [tf.shape(im1)[1], tf.shape(im2)[2]])
+    # flow = tf.image.resize_images(flow_course, [tf.shape(im1)[1], tf.shape(im2)[2]])
+    # im1_warped = tfa.image.dense_image_warp(im1, flow )
+    im1_warped = tf.keras.layers.Lambda(lambda a: tfa.image.dense_image_warp(a[0], a[1]))((im1, flow))
+
     res = convnet(im1_warped, im2, flow)
     flow_fine = res + flow
 
-    im1_warped_fine = tfa.image.dense_image_warp(im1, flow_fine)
+    # im1_warped_fine = tfa.image.dense_image_warp(im1, flow_fine)
+    im1_warped_fine = tf.keras.layers.Lambda(lambda a: tfa.image.dense_image_warp(a[0], a[1]))((im1, flow_fine))
+    
     loss_layer = tf.math.reduce_mean(tf.math.squared_difference(im1_warped_fine, im2))
 
     return loss_layer, flow_fine
@@ -44,42 +52,30 @@ def optical_flow(im1_4, im2_4, bach, h, w):
     im2_1 = AveragePooling2D(pool_size=2, strides=2, padding='same')(im2_2)
     im2_0 = AveragePooling2D(pool_size=2, strides=2, padding='same')(im2_1)
 
-    flow_zero = tf.zeros((1, h//16, w//16, 2), dtype=tf.dtypes.float32)
+    # print(bach)
+    flow_zero = tf.zeros((bach, h//16, w//16, 2), dtype=tf.dtypes.float32)
+    # flow_zero = tf.zeros_like(im2_0)
 
     loss_0, flow_0 = loss(flow_zero, im1_0, im2_0)
     loss_1, flow_1 = loss(flow_0, im1_1, im2_1)
     loss_2, flow_2 = loss(flow_1, im1_2, im2_2)
     loss_3, flow_3 = loss(flow_2, im1_3, im2_3)
     loss_4, flow_4 = loss(flow_3, im1_4, im2_4)
-
-    return flow_4, loss_0, loss_1, loss_2, loss_3, loss_4
+  
+    return flow_4
     # return im2_0
     
 
-class OpticalFlow(layers.Layer):
-    def __init__(self, trainable=True, name=None, dtype=None, dynamic=False, **kwargs):
-        super().__init__(trainable=trainable, name=name, dtype=dtype, dynamic=dynamic, **kwargs)
-
-    def build(self, input_shape):
-
-        self.bach = input_shape[0][0]
-        self.h = input_shape[0][1]
-        self.w = input_shape[1][2]
-
-        return super().build(input_shape)
-
-    def call(self, inputs, *args, **kwargs):
-        flow_4, loss_0, loss_1, loss_2, loss_3, loss_4 = optical_flow(inputs[0], inputs[1], self.bach, self.h, self.w)
-        return flow_4
-
-
 # MV_analysis defaults
+
 def encoder(input, num_filters=128, kernel_size=3, M=2):
     x = tfc.SignalConv2D(num_filters, (kernel_size, kernel_size), corr=True, strides_down=2, padding="same_zeros", use_bias=True, activation=tfc.GDN())(input)
     x = tfc.SignalConv2D(num_filters, (kernel_size, kernel_size), corr=True, strides_down=2, padding="same_zeros", use_bias=True, activation=tfc.GDN())(x)
     x = tfc.SignalConv2D(num_filters, (kernel_size, kernel_size), corr=True, strides_down=2, padding="same_zeros", use_bias=True, activation=tfc.GDN())(x)
     x = tfc.SignalConv2D(M, (kernel_size, kernel_size), corr=True, strides_down=2, padding="same_zeros", use_bias=True, activation=tfc.GDN())(x)
+    
     return x
+
 
 def decoder(input, num_filters=128, kernel_size=3, M=2):
     x = tfc.SignalConv2D(num_filters, (kernel_size, kernel_size), corr=False, strides_up=2, padding="same_zeros", use_bias=True, activation=tfc.GDN(inverse=True))(input)
