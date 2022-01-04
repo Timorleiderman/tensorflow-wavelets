7f4117361ed766c863de2fb40bd228cf4ea8d275
Timor Leiderman <timorleiderman@gmail.com>
2022-01-04 08:41:54 +0200

    Timor: added examples for training

--- Development/OpenDVC/load.py
+++ Development/OpenDVC/load.py
@@ -1,6 +1,15 @@
+from os import path
 import imageio
 import numpy as np
 
+def load_local_data(data, frames, bach_size, height, width, channels, folder):
+    for bch in range(bach_size):
+        path = folder[np.random.randint(len(folder))] + '/'
+        bb = np.random.randint(0, 447 - 256)
+        for f in range(frames):
+            img = imageio.imread(path + 'f' + str(f + 1).zfill(3) + '.png')
+            data[f, bch, 0 : height, 0 : width, 0 : channels] = img[0 : height, bb : bb + width, 0 : channels]
+    return data
 
 def load_data(data, frames, batch_size, Height, Width, Channel, folder, I_QP):
 
@@ -28,15 +37,17 @@ if __name__=="__main__":
     import load
 
     batch_size = 4
-    Height = 256
-    Width = 256
+    Height = 240
+    Width = 240
     Channel = 3
     lr_init = 1e-4
     frames=2
     I_QP=27
 
-    folder = np.load("/mnt/WindowsDev/Developer/tensorflow-wavelets/folder_cloud.npy")
+    # folder = np.load("/mnt/WindowsDev/Developer/tensorflow-wavelets/folder_cloud.npy")
+    folder = ["/workspaces/tensorflow-wavelets/Development/OpenDVC/BasketballPass"]
     #a = load.load_data()
     data = np.zeros([frames, batch_size, Height, Width, Channel])
-    data = load_data(data, frames, batch_size, Height, Width, Channel, folder, I_QP)
+    data - load_local_data(data, frames, batch_size, Height, Width, Channel, folder)
+    # data = load_data(data, frames, batch_size, Height, Width, Channel, folder, I_QP)
     print("Data Load done! ...")
