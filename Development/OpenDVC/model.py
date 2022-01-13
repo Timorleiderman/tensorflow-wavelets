90ed9cb035f256c8abb2c12845420ddc2519f747
Timor <Timorleiderman@gmail.com>
2022-01-14 00:26:29 +0200

    Update model.py

--- Development/OpenDVC/model.py
+++ Development/OpenDVC/model.py
@@ -3,7 +3,7 @@ import tensorflow_addons as tfa
 import tensorflow_compression as tfc
 import motion
 import numpy as np
-
+import load
 
 class AnalysisTransform(tf.keras.Sequential):
   """The analysis transform."""
@@ -28,15 +28,21 @@ class SynthesisTransform(tf.keras.Sequential):
 class OpenDVC(tf.keras.Model):
     """Main model class."""
 
-    def __init__(self, num_filters=128):
+    def __init__(self, width=240, height=240, batch_size=4, num_filters=128):
         super().__init__()
         self.mv_analysis_transform = AnalysisTransform(num_filters, kernel_size=3, M=128)
         self.mv_synthesis_transform = SynthesisTransform(num_filters, kernel_size=3)
         self.res_analysis_transform = AnalysisTransform(num_filters, kernel_size=5, M=128)
         self.res_synthesis_transform = SynthesisTransform(num_filters, kernel_size=5, M=3)
         self.prior = tfc.NoisyDeepFactorized(batch_shape=(num_filters,))
-        self.build((2, None, 240, 240, 3))
         
+
+        self.width = width
+        self.height = height
+        self.batch_size = batch_size
+
+        self.l = 256
+        self.build((2, batch_size, width, height, 3))
 
 
     def call(self, x, training):
@@ -50,42 +56,55 @@ class OpenDVC(tf.keras.Model):
 
         # optical flow
         
-        flow_tensor = motion.optical_flow(Y0_com, Y1_raw, 1)
+        flow_tensor = motion.optical_flow(Y0_com, Y1_raw, self.batch_size)
         flow_latent = self.mv_analysis_transform(flow_tensor)
-
         flow_latent_hat, MV_likelihoods_bits = entropy_model_mv(flow_latent, training=training)
         flow_hat = self.mv_synthesis_transform(flow_latent_hat)
+
         Y1_warp = tfa.image.dense_image_warp(Y0_com, flow_hat )
-
         MC_input = tf.concat([flow_hat, Y0_com, Y1_warp], axis=-1)
-
         Y1_MC = motion.MotionCompensation(MC_input)
+
         Res = Y1_raw - Y1_MC
+
         res_latent = self.res_analysis_transform(Res)
         res_latent_hat, Res_likelihoods_bits = entropy_model_res(res_latent, training=training)
         Res_hat = self.res_synthesis_transform(res_latent_hat)
-
         Y1_com = Res_hat + Y1_MC
 
-        # Total number of bits divided by total number of pixels.
-        num_pixels = tf.cast(tf.reduce_prod(tf.shape(Y0_com)[:-1]), MV_likelihoods_bits.dtype)
-        bpp = tf.reduce_sum(MV_likelihoods_bits) / num_pixels
-        # Mean squared error across pixels.
-        mse = tf.reduce_mean(tf.math.squared_difference(Y0_com, Y1_warp))
-        # The rate-distortion Lagrangian.
-        loss = bpp + 0.01 * mse
-        return loss, bpp, mse
+        train_bpp_MV = tf.reduce_sum(tf.math.log(MV_likelihoods_bits)) / (-np.log(2) * self.height * self.width * self.batch_size)
+        train_bpp_Res = tf.reduce_sum(tf.math.log(Res_likelihoods_bits)) / (-np.log(2) * self.height * self.width * self.batch_size)
+
+        total_mse = tf.reduce_mean(tf.math.squared_difference(Y1_com, Y1_raw))
+        warp_mse = tf.reduce_mean(tf.math.squared_difference(Y1_warp, Y1_raw))
+        MC_mse = tf.reduce_mean(tf.math.squared_difference(Y1_raw, Y1_MC))
+
+        psnr = 10.0*tf.math.log(1.0/total_mse)/tf.math.log(10.0)
+        
+        train_loss_total = self.l * total_mse + (train_bpp_MV + train_bpp_Res)
+        train_loss_MV = self.l * warp_mse + train_bpp_MV
+        train_loss_MC = self.l * MC_mse + train_bpp_MV
+
+        return train_loss_total, train_loss_MV, train_loss_MC, total_mse, warp_mse, MC_mse, psnr
 
     def train_step(self, x):
         with tf.GradientTape() as tape:
-            loss, bpp, mse = self(x, training=True)
+            train_loss_total, train_loss_MV, train_loss_MC, total_mse, warp_mse, MC_mse, psnr = self(x, training=True)
         variables = self.trainable_variables
-        gradients = tape.gradient(loss, variables)
-        self.optimizer.apply_gradients(zip(gradients, variables))
-        self.loss.update_state(loss)
-        self.bpp.update_state(bpp)
-        self.mse.update_state(mse)
-        return {m.name: m.result() for m in [self.loss, self.bpp, self.mse]}
+        gradients = tape.gradient(train_loss_total, variables)
+
+        # self.train_MV_opt.apply_gradients(zip(gradients, variables))
+
+        # self.train_loss_total.update_state(train_loss_total)
+        # self.train_loss_MV.update_state(train_loss_MV)
+        # self.train_loss_MC.update_state(train_loss_MC)
+        # self.psnr.update_state(psnr)
+        # self.total_mse.update_state(total_mse)
+        # self.warp_mse.update_state(warp_mse)
+        # self.MC_mse.update_state(MC_mse)
+
+    
+        return {m.name: m.result() for m in [self.train_loss_total, self.train_loss_MV, self.train_loss_MC, self.psnr, self.total_mse, self.warp_mse, self.MC_mse]}
 
 
     @tf.function(input_signature=[
@@ -94,7 +113,7 @@ class OpenDVC(tf.keras.Model):
     def compress(self, flow_tensor):
         """Compresses an image."""
         # Add batch dimension and cast to float.
-        x = tf.expand_dims(x, 0)
+        x = tf.expand_dims(flow_tensor, 0)
         x = tf.cast(x, dtype=tf.float32)
         flow_latent = self.mv_analysis_transform(x)
         # Preserve spatial shapes of both image and latents.
@@ -103,16 +122,22 @@ class OpenDVC(tf.keras.Model):
         return self.entropy_model.compress(flow_latent), x_shape, y_shape
 
     def compile(self, **kwargs):
-        super().compile(
-            loss=None,
-            metrics=None,
-            loss_weights=None,
-            weighted_metrics=None,
-            **kwargs,
-        )
-        self.loss = tf.keras.metrics.Mean(name="loss")
-        self.bpp = tf.keras.metrics.Mean(name="bpp")
-        self.mse = tf.keras.metrics.Mean(name="mse")
+        super().compile(loss=None, metrics=None, loss_weights=None, weighted_metrics=None, **kwargs,)
+        
+        self.train_loss_total = tf.keras.metrics.Mean(name="train_loss_total")
+        self.train_loss_MV = tf.keras.metrics.Mean(name="train_loss_MV")
+        self.train_loss_MC = tf.keras.metrics.Mean(name="train_loss_MC")
+        self.psnr = tf.keras.metrics.Mean(name="psnr")
+        self.total_mse = tf.keras.metrics.Mean(name="total_mse")
+        self.warp_mse = tf.keras.metrics.Mean(name="warp_mse")
+        self.MC_mse = tf.keras.metrics.Mean(name="MC_mse")
+
+        learning_rate = 1e-4
+        self.train_MV_opt = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+        train_MC = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+        train_total = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+        aux_optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate*10.0)
+        aux_optimizer2 = tf.keras.optimizers.Adam(learning_rate=learning_rate*10.0)
 
     def fit(self, *args, **kwargs):
         retval = super().fit(*args, **kwargs)
@@ -124,5 +149,22 @@ class OpenDVC(tf.keras.Model):
 if __name__ == "__main__":
     print("gg")
     model = OpenDVC()
-    model.summary()
-    
+    # model.summary()
+    model.compile()
+
+    import load
+
+    folder = ["/workspaces/tensorflow-wavelets/Development/OpenDVC/BasketballPass"]
+
+    batch_size = 4
+    Height = 240
+    Width = 240
+    Channel = 3
+    lr_init = 1e-4
+    frames=2
+    I_QP=27
+
+    data = np.zeros([frames, batch_size, Height, Width, Channel])
+    data - load.load_local_data(data, frames, batch_size, Height, Width, Channel, folder)
+    model.fit(data, epochs=4, steps_per_epoch=1, verbose=1, )    
+   
