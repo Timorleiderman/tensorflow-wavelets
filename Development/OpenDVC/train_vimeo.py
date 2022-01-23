0a07ab4376c05cea8facc26268eb65c57aee4a93
Timor <Timorleiderman@gmail.com>
2022-01-23 07:43:31 +0200

    Update train_vimeo.py

--- Development/OpenDVC/train_vimeo.py
+++ Development/OpenDVC/train_vimeo.py
@@ -6,39 +6,44 @@ import matplotlib.pyplot as plt
 
 tf.executing_eagerly()
 
-batch_size = 2
+batch_size = 1
 EPOCHS = 50
 Height = 240
 Width = 240
 Channel = 3
 lr_init = 1e-4
-samples=500
+samples=1000
 I_QP=27
 
 model = OpenDVC.OpenDVC(width=Width, height=Height, batch_size=batch_size, num_filters=128)
 # model.summary()
 model.compile()
+print("* [Model compiled]...")
 args = OpenDVC.Arguments()
-folder = np.load("/mnt/WindowsDev/Developer/tensorflow-wavelets/folder_cloud.npy")
 
-data = np.zeros([samples, batch_size, Height, Width, Channel])
-# data = load_local_data(data, frames, batch_size, Height, Width, Channel, folder)
-data = load.load_data_7vimeo(data, samples, Height, Width, Channel, folder, I_QP)
-dataset = tf.data.Dataset.from_tensor_slices(data).batch(batch_size)
+# folder = np.load("/mnt/WindowsDev/Developer/tensorflow-wavelets/folder_cloud.npy")
+# data = load.load_data_vimeo90k(samples, batch_size, Height, Width, Channel, folder, I_QP)
+data = load.load_data_vimeo90k("/mnt/WindowsDev/Developer/tensorflow-wavelets/folder_cloud.npy",
+                                samples, Height, Width, Channel, I_QP)
+
+dataset = tf.data.Dataset.from_tensor_slices(data)
 
 
-model.fit(dataset,
-            epochs=EPOCHS, 
-            verbose=1, 
-            callbacks=
+print("Going to fit")
+model.fit(
+        dataset,
+        epochs=EPOCHS, 
+        verbose=1, 
+        callbacks=
             [
             OpenDVC.MemoryCallback(),
-            tf.keras.callbacks.ModelCheckpoint(filepath=args.model_checkpoints, save_weights_only=True, save_freq='epoch', monitor='train_loss_total', mode='max', save_best_only=True), 
+            tf.keras.callbacks.ModelCheckpoint(filepath=args.model_checkpoints, save_weights_only=True, save_freq='epoch', monitor='train_loss_MV', mode='max', save_best_only=True), 
             tf.keras.callbacks.TerminateOnNaN(),
             tf.keras.callbacks.TensorBoard(log_dir=args.backup_restore, histogram_freq=0, update_freq="epoch"),
             tf.keras.callbacks.experimental.BackupAndRestore(args.backup_restore),
             ],
-            )  
+
+        )  
 
 print("Done Training ...")
 tf.saved_model.save(model, args.model_save)
