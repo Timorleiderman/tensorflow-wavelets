8984ae4245f7d0efa5038f046ac12338cf18f1fb
Timor <Timorleiderman@gmail.com>
2022-01-23 07:43:51 +0200

    Update test.py

--- Development/OpenDVC/test.py
+++ Development/OpenDVC/test.py
@@ -1,6 +1,6 @@
 import OpenDVC
 import numpy as np
-from load import load_local_data, load_data
+import load
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
@@ -15,15 +15,21 @@ lr_init = 1e-4
 frames=6
 I_QP=27
 
-folder = np.load("/mnt/WindowsDev/Developer/tensorflow-wavelets/folder_cloud.npy")
-
-data = np.zeros([frames, batch_size, Height, Width, Channel])
-# data = load_local_data(data, frames, batch_size, Height, Width, Channel, folder)
-data = load_data(data, frames, batch_size, Height, Width, Channel, folder, I_QP)
-dataset = tf.data.Dataset.from_tensor_slices(data).batch(2)
+path = load.load_random_path("/mnt/WindowsDev/Developer/tensorflow-wavelets/folder_cloud.npy")
 
 arg = OpenDVC.Arguments()
+
 model = tf.saved_model.load(arg.model_save)
 
-OpenDVC.compress(arg, model)
-OpenDVC.decompress(arg, model)
+i_frame = path + 'im1' + '.png'
+p_frame = path + 'im2' + '.png'
+out_bin = "/workspaces/tensorflow-wavelets/Development/OpenDVC/Test_com/test.bin"
+out_decom = "/workspaces/tensorflow-wavelets/Development/OpenDVC/Test_com/testdcom.png"
+p_on_test = "/workspaces/tensorflow-wavelets/Development/OpenDVC/Test_com/test_p_frame.png"
+i_on_test = "/workspaces/tensorflow-wavelets/Development/OpenDVC/Test_com/test_i_frame.png"
+
+OpenDVC.write_png(p_on_test, OpenDVC.read_png_crop(p_frame, 240, 240))
+OpenDVC.write_png(i_on_test, OpenDVC.read_png_crop(i_frame, 240, 240))
+
+OpenDVC.compress(model, i_frame, p_frame,out_bin, 240, 240)
+OpenDVC.decompress(model, i_frame, out_bin, out_decom, 240, 240)
