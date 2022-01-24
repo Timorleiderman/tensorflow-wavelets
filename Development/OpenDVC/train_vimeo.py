7a2754d405e5e4d7fd76371dd38c71b7ebfb7154
Timor <Timorleiderman@gmail.com>
2022-01-24 23:19:31 +0200

    Update train_vimeo.py

--- Development/OpenDVC/train_vimeo.py
+++ Development/OpenDVC/train_vimeo.py
@@ -12,7 +12,7 @@ Height = 240
 Width = 240
 Channel = 3
 lr_init = 1e-4
-samples = 800
+samples = 1000
 I_QP=27
 
 model = OpenDVC.OpenDVC(width=Width, height=Height, batch_size=batch_size, num_filters=128)
@@ -28,7 +28,7 @@ data = load.load_data_vimeo90k("/mnt/WindowsDev/Developer/tensorflow-wavelets/fo
 
 dataset = tf.data.Dataset.from_tensor_slices(data)
 
-model.load_weights(args.model_checkpoints)
+# model.load_weights(args.model_checkpoints)
 # model.load_weights(args.model_checkpoints_me)
 # model.load_weights(args.model_checkpoints_mv)
 # model.load_weights(args.model_checkpoints_mc)
@@ -41,13 +41,13 @@ hist = model.fit(
         callbacks=
             [
             OpenDVC.MemoryCallback(),
-            # tf.keras.callbacks.ModelCheckpoint(filepath=args.model_checkpoints_me, save_weights_only=True, save_freq='epoch',monitor="train_loss_ME",mode='min',  save_best_only=True, verbose=2), 
-            tf.keras.callbacks.ModelCheckpoint(filepath=args.model_checkpoints_mv, save_weights_only=True, save_freq='epoch',monitor="train_loss_MV",mode='min',  save_best_only=True, verbose=2), 
-            tf.keras.callbacks.ModelCheckpoint(filepath=args.model_checkpoints_mc, save_weights_only=True, save_freq='epoch',monitor="train_loss_MC",mode='min',  save_best_only=True, verbose=2), 
+            tf.keras.callbacks.ModelCheckpoint(filepath=args.model_checkpoints_me, save_weights_only=True, save_freq='epoch', monitor="train_loss_ME", mode='min',  save_best_only=True, verbose=2), 
+            # tf.keras.callbacks.ModelCheckpoint(filepath=args.model_checkpoints_mv, save_weights_only=True, save_freq='epoch',monitor="train_loss_MV",mode='min',  save_best_only=True, verbose=2), 
+            # tf.keras.callbacks.ModelCheckpoint(filepath=args.model_checkpoints_mc, save_weights_only=True, save_freq='epoch',monitor="train_loss_MC",mode='min',  save_best_only=True, verbose=2), 
 
             tf.keras.callbacks.TerminateOnNaN(),
             tf.keras.callbacks.TensorBoard(log_dir=args.backup_restore, histogram_freq=0, update_freq="epoch"),
-            tf.keras.callbacks.experimental.BackupAndRestore(args.backup_restore),
+            # tf.keras.callbacks.experimental.BackupAndRestore(args.backup_restore),
             ],
 
         )  
